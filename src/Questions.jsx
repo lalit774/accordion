0da893225d9a46385a7bab93b5b09c89a488b149
lalit kumar <lalit.kumar@socialinnovation.ca>
2023-10-14 18:34:56 -0400

    update

---
+++ src/Questions.jsx
@@ -0,0 +1,19 @@
+import Question from './Question'
+
+export const Questions = ({ questions, activeId, toggleQuestion }) => {
+  return (
+    <section className="container">
+      <h1>Questions</h1>
+      {questions.map((question) => {
+        return (
+          <Question
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          />
+        )
+      })}
+    </section>
+  )
+}
